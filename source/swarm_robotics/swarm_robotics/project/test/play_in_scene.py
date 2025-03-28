b4cc2be00e00d27fda0603873611d202c4d845d0
LFM <531672907@qq.com>
2025-03-28 14:01:34 +0800

    modify

--- source/swarm_robotics/swarm_robotics/project/test/play_in_scene.py
+++ source/swarm_robotics/swarm_robotics/project/test/play_in_scene.py
@@ -26,7 +26,7 @@ from isaaclab.app import AppLauncher
 # add argparse arguments
 parser = argparse.ArgumentParser(description="Tutorial on inferencing a policy on an H1 robot in a warehouse.")
 parser.add_argument("--checkpoint",
-                    default="/project/EXP/Simulation/logs/rsl_rl/anymal_d_rough/2025-03-12_19-12-33/2025-03-05_14-36-30/exported/policy.pt",
+                    default="/project/EXP/Simulation/logs/rsl_rl/anymal_d_rough/2025-03-12_19-12-33/2025-03-17_21-11-35/exported/policy.pt",
                     type=str, help="Path to model checkpoint exported as jit.", required=False)
 
 # append AppLauncher cli args
@@ -50,7 +50,7 @@ from isaaclab.terrains import TerrainImporterCfg
 # from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
 
 from swarm_robotics.tasks.locomotion.velocity.config.h1.rough_env_cfg import H1RoughEnvCfg_PLAY
-# from isaaclab_tasks.manager_based.locomotion.velocity.config.anymal_c.rough_env_cfg import AnymalCRoughEnvCfg_PLAY
+from isaaclab_tasks.manager_based.locomotion.velocity.config.anymal_c.rough_env_cfg import AnymalCRoughEnvCfg_PLAY
 
 
 """
@@ -86,10 +86,11 @@ def main():
     file_content = omni.client.read_file(policy_path)[2]
     file = io.BytesIO(memoryview(file_content).tobytes())
     policy = torch.jit.load(file)
-    env_cfg = H1RoughEnvCfg_PLAY()
-    
+
+    # env_cfg = H1RoughEnvCfg_PLAY()
+    env_cfg = AnymalCRoughEnvCfg_PLAY()
     # env_cfg = AnymalCRoughEnvCfg_PLAY()
-    env_cfg.scene.num_envs = 20
+    env_cfg.scene.num_envs = 4
     env_cfg.curriculum = None
     env_cfg.scene.terrain = TerrainImporterCfg(
         prim_path="/World/ground",
